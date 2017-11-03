0c057a02832e4dc8c1f30de380df1c933c0868f5
sujeets207 <sujeets207@hotmail.com>
2017-11-03 17:47:09 +0530

    added VILTRIBE.cpp

--- CHALLENGES/NOV17/VILTRIBE.cpp
+++ CHALLENGES/NOV17/VILTRIBE.cpp
@@ -2,32 +2,31 @@
 using namespace std;
 int main(){
     long long int t;
-    cin>>t;
-    while(t--){
+    cin>>test;
+    while(test--){
         string s;
         cin>>s;
-        int aa=0,bb=0,counter=0;
-        char c='.';
-        bool flag1=false,flag2=false;
-        for(int i=0;i<s.length();i++){
+        long long int aa=0,bb=0,count=0;
+        char ch='.';
+        for(long long int i=0;i<s.length();i++){
         	if(s[i]=='A'){
         		aa++;
-        		if(c=='A'){
-        			aa+=counter;	
+        		if(ch=='A'){
+        			aa+=count;	
         		}
         		counter=0;
         	}
         	if(s[i]=='B'){
         		bb++;
-        		if(c=='B'){
-        			bb+=counter;
+        		if(ch=='B'){
+        			bb+=count;
         		}
         		counter=0;
         	}
         	if(s[i]=='A' || s[i]=='B')
-        		c=s[i];
+        		ch=s[i];
         	if(s[i]=='.')
-        		counter++;
+        		count++;
         	//cout<<aa<<" "<<bb<<endl;
         }
         cout<<aa<<" "<<bb<<"\n";
