a5ab5e894ae4b4b21005d2aea283f72b343cbd89
powerfist01 <sujeets207@gmail.com>
2021-10-03 12:37:22 +0530

    added stack in stl

---
+++ STL/stacks.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+int main( )
+{
+    stack <int> s;  // declaration of stack
+
+    //inserting 5 elements in stack from 0 to 4.
+    for(int i = 0;i < 5; i++)
+    {
+        s.push( i ) ;
+    }
+
+    //Now the stack is {0, 1, 2, 3, 4}
+
+    //size of stack s
+    cout<<”Size of stack is: ” <<s.size( )<<endl;
+
+    //accessing top element from stack, it will be the last inserted element.
+    cout<<”Top element of stack is: ” <<s.top( ) <<endl ;
+
+    //Now deleting all elements from stack 
+    for(int i = 0;i < 5;i++)
+    {
+        s.pop( );
+    }
+
+    //Now stack is empty,so empty( ) function will return true.
+
+    if(s.empty())
+    {
+        cout <<”Stack is empty.”<<endl;
+    }
+    else
+    {
+        cout <<”Stack is Not empty.”<<endl;
+    }
+
+    return 0;
+
+}
